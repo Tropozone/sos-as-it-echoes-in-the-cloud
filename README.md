89f3d7ce1dd81ba60385614feff912c4ee097f5d
Claire Aoi <soundslike003@gmail.com>
2021-02-13 14:16:56 +0800

    First Scripts Utils

---
+++ README.md
@@ -0,0 +1,1 @@
+# the sound of speech as it echoes in the cloud
--- utils/totxt.py
+++ utils/totxt.py
@@ -5,6 +5,8 @@ import shutil
 import os, fnmatch
 import os.path
 import pdftotext
+import ebooklib
+from ebooklib import epub
 
 data_folder='../../Google Drive/data/' #Update path where data is if needed
 merged_txt_path='./data/all_txt_1302.txt'#Update Path and Name merged data.
@@ -27,30 +29,58 @@ def pdftotxt():
         with open(pdffile, "rb") as f:
             pdf = pdftotext.PDF(f)
         txtfile=pdffile.replace(".pdf",".txt")
-        print("Converted to txt:"+txtfile)
+        print("Converting to txt:"+txtfile)
         with open(txtfile, 'w') as f:
             f.write("\n\n".join(pdf))
     print("Converted all pdf to txt.")
 #pdftotxt()
 
 #####------------CONVERSION ALL EPUB to TXT
+def epub2thtml(epub_path):
+    book = epub.read_epub(epub_path)
+    chapters = []
+    for item in book.get_items():
+        if item.get_type() == ebooklib.ITEM_DOCUMENT:
+            chapters.append(item.get_content())
+    return chapters
+
+
 def epubtotxt():
-    print("Start conversion epub to txt of the pdf.")
-    pass
+    print("Start conversion epub to txt.")
+    for epubfile in findfiles(data_folder, '*.epub'):
+        filename = os.path.basename(epubfile)
+        print(filename)
+        chapters=epub2thtml(epubfile)
+        print(chapters)
+        txtfile=epubfile.replace(".epub",".txt")
+        print("Converting to txt:"+txtfile)
+        with open(txtfile, 'w') as f:
+            f.write("\n\n".join(chapters))
+    print("Converted all epub to txt.")
+
+#epubtotxt()#ISSUE currently
 
 #####------------CONVERSION ALL DJVU to TXT
 
 def djvutotxt():
-    print("Start conversion djvu  to txt of the pdf.")
-    pass
+    print("Start conversion djvu to txt.")
+    for djvufile in findfiles(data_folder, '*.djvu'):
+        filename = os.path.basename(epubfile)
+        print(filename)
+        txtfile=epubfile.replace(".epub",".txt")
+        print("Converting to txt:"+txtfile)
+        pass
 
 #####----------MERGE TXT into newfile
-print("Start merging")
-#Combine your uploaded files into one called merged.txt
-#with open(merged_txt_path, 'a+') as outfile:
-#    for filename in glob.glob(data_folder+'*.txt'):
-#        with open(filename, 'r') as readfile:
-#            print("starting the file"+filename)
-#            shutil.copyfileobj(readfile, outfile)
-#            print("Added the txt file"+filename)
-#    print(f'merged.txt created in {data_folder}data/')
+
+def mergetxt():
+    print("Start merging")
+    #Combine your uploaded files into one called merged.txt
+    with open(merged_txt_path, 'a+') as outfile:
+        for filename in glob.glob(data_folder+'*.txt'):
+            with open(filename, 'r') as readfile:
+                print("starting the file"+filename)
+                shutil.copyfileobj(readfile, outfile)
+                print("Added the txt file"+filename)
+        print(f'merged Text created in {data_folder}data/')
+#mergetxt()
